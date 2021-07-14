dcd313e186e6526da7650e1e75613cd364963b0a
lahiruCMD <55289703+lahiru011@users.noreply.github.com>
2021-07-14 19:31:30 +0530

    adding homepage content

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -2,12 +2,15 @@ import React from "react";
 import "../../App.css";
 // import Cards from '../Cards';
 import HomeSection from '../HomeSection';
+import PriceSection from "../pricing/PriceSection";
 // import Footer from '../Footer';
 
 function Home() {
   return (
-    <>
-      <HomeSection />
+    <>  
+      <HomeSection/>
+      <PriceSection/>
+      
     </>
   );
 }
