a5179c22789f7680e5ab37ab9a74d425d962a5d3
TypicalCoderr <desilvalahiru4@gmail.com>
2021-08-01 20:41:03 +0530

    commit

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -5,23 +5,75 @@ import { Link } from 'react-router-dom';
 import './HomeSection.css';
 import backgroundImage from '../images/bg2.gif';
 
-function HomeSection() {
+function HomeSection({
+  lightBg,
+  topLine,
+  lightText,
+  lightTextDesc,
+  headline,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  imgStart,
+}) {
   return (
+    // <div
+    //   className="home-container"
+    //   style={{ background: `url(${backgroundImage})` }}
+    // >
+    //   <div className="get-started">
+    //     <h1>Start reading, start inspiring</h1>
+    //     <p>What are you waiting for?</p>
+    //     <div className="hero-btns">
+    //       <Button
+    //         className="btns"
+    //         buttonStyle="btn--outline"
+    //         buttonSize="btn--large"
+    //       >
+    //         GET STARTED
+    //       </Button>
+    //     </div>
+    //   </div>
+    // </div>
     <div
-      className="home-container"
-      style={{ background: `url(${backgroundImage})` }}
+      className={lightBg ? "home__hero-section" : "home__hero-section darkBg"}
     >
-      <div className="get-started">
-        <h1>Start reading, start inspiring</h1>
-        <p>What are you waiting for?</p>
-        <div className="hero-btns">
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            GET STARTED
-          </Button>
+      <div className="container">
+        <div
+          className="row home__hero-row"
+          style={{
+            display: "flex",
+            flexDirection: imgStart === "start" ? "row-reverse" : "row",
+          }}
+        >
+          <div className="col">
+            <div className="home__hero-text-wrapper">
+              <div className="top-line">{topLine}</div>
+              <h1 className={lightText ? "heading" : "heading dark"}>
+                {headline}
+              </h1>
+              <p
+                className={
+                  lightTextDesc
+                    ? "home__hero-subtitle"
+                    : "home__hero-subtitle dark"
+                }
+              >
+                {description}
+              </p>
+              <Link to="/sign-up">
+                <Button buttonSize="btn--wide" buttonColor="blue">
+                  {buttonLabel}
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="col">
+            <div className="home__hero-img-wrapper">
+              <img src={img} alt={alt} className="home__hero-img" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
