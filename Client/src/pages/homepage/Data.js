378533b00b4b57f6d8b243235555b81668e26e18
TypicalCoderr <desilvalahiru4@gmail.com>
2021-08-29 03:29:18 +0530

    [implemented] homePage

---
+++ Client/src/pages/homepage/Data.js
@@ -0,0 +1,13 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Explore",
+  headline: "Unlimited Books & Videos",
+  description:
+    "Get access to our exclusive plans that allows you to  access unlimited books and videos everyday.",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img: "images/book-lover.svg",
+  alt: "Book Reader",
+};
