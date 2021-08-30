565a6224b93705862b9d072b8069be2f6bae7b5f
TypicalCoderr <desilvalahiru4@gmail.com>
2021-08-31 01:04:52 +0530

    [+] creating database

--- Server/server.js
+++ Server/server.js
@@ -1,2 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+const db = require('./models');
+
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
+
+
+db.sequelize.sync().then(()=>{
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
+
+});
+
+
