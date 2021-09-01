80d0b1c23d7d0f3438a068acb3daa074d27fc385
TypicalCoderr <desilvalahiru4@gmail.com>
2021-09-02 01:07:52 +0530

    [implemented] login function

--- Client/src/pages/Login/Login.js
+++ Client/src/pages/Login/Login.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import loginImg from "../../assets/images/login-form.svg";
+import axios from "axios";
 
-function login() {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    const data = { email: email, password: password };
+    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+      console.log(response.data);
+    });
+  };
+
   return (
     <div className="container-login">
       <div className="row">
@@ -13,22 +24,39 @@ function login() {
           <hr />
           <p style={{ color: "gray" }}> .Hi there! good to see you again</p>
 
-          <form className="login-form-details">
-            <label>:Email</label>
-            <input type="Text" />
-            <label>:Password</label>
-            <input type="password" />
+          <div className="login-form-details">
+            <input
+              type="Text"
+              name="email"
+              placeholder=" Email"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder=" Password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <hr />
+            <p style={{ color: "gray" }}>
+              .Forget password? <a href="#">Click here</a>
+            </p>
+            <hr />
 
-            <input type="submit" value="LOGIN"></input>
+            <button onClick={login}>LOGIN</button>
             <hr />
             <div className="signup-link">
               <p>
                 Don't have an account? <a href="/SignUp">SignUp</a>
               </p>
             </div>
-          </form>
+          </div>
         </div>
-        <div className="col-1">
+        <div className="img-login">
           <img className="img-form" src={loginImg} alt="" />
         </div>
       </div>
@@ -36,4 +64,4 @@ function login() {
   );
 }
 
-export default login;
+export default Login;
