c051eb21017d0a477357f11256e4834733ed2ead
TypicalCoderr <desilvalahiru4@gmail.com>
2021-09-02 01:11:00 +0530

    [+] server with sequelize

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
-const db = require('./models');
+const db = require("./models");
 
+app.use(express.json());
+//routers
 const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 
-
-db.sequelize.sync().then(()=>{
+db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
   });
-
 });
-
-
