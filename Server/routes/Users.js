c6a072f26b777701988420d2ecdb4c564c9a34b4
TypicalCoderr <desilvalahiru4@gmail.com>
2021-09-02 01:08:44 +0530

    [+] register & login routes added

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const {Users} = require("../models");
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
-    const { firstName, lastName, email, age, phoneNo, password} = req.body;
+  const { firstName, lastName, email, age, phoneNo, password } = req.body;
+  
+  bcrypt.hash(password, 10).then((hash) =>{
+      Users.create({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          age: age,
+          phoneNo: phoneNo,
+          password: hash,
+
+      });
+      res.send("success!");
+  });
+});
+
+router.post('/login', async (req, res)=>{
+    const {email,password} = req.body;
+    const user = await Users.findOne({where: { email: email}});
+    if (!user) res.json({ error: "User does not exist"});
+
+    bcrypt.compare(password, user.password).then((match)=>{
+        if(!match) res.json({ error: "Wrong username or password combination"});
+        res.json("You Logged in!!");
+
+    });
 });
 
 module.exports = router;
