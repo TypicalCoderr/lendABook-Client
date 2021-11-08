1a5790582ca97733d0338d6df371f6baca6cd3c7
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-09 01:36:42 +0530

    [+] signup,login rotues

--- V2/Client/client/src/App.js
+++ V2/Client/client/src/App.js
@@ -1,18 +1,31 @@
 import "./App.scss";
-import Navbar from "./components/navbar/navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Fragment } from "react";
+import axios from "axios";
+
 import Home from "./pages/home/home";
+import Login from "./pages/login/login";
+import Signup from "./pages/signUp/signUp";
+import UserImgUpload from "./pages/signUp/imageForm";
+
+
+axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Fragment>
+      
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/user/login" element={<Login />} />
+              <Route exact path="/user/register" element={<Signup />} />
+              <Route exact path="/img-upload" element={<UserImgUpload />} />
+            </Routes>
+          </Router>
+       
+      </Fragment>
     </>
   );
 }
