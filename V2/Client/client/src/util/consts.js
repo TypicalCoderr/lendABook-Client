24293be827062b23327bfa1c4b0315cd23377486
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-13 22:30:29 +0530

    [+] book categories

---
+++ V2/Client/client/src/util/consts.js
@@ -0,0 +1,8 @@
+exports.BOOK_CATRGORIES = [
+    { id: "classic", name: "Classic"},
+    { id: "comic ", name: "Comic "},
+    { id: "fiction", name: "Fiction"},
+    { id: "horror", name: "Horror"},
+    { id: "sci_fi", name: "Sci-Fi"},
+    { id: "biographies ", name: "Biographies "},
+]
