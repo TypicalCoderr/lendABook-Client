099b0c53ffd9ef677dae20759fd22f0dc7a17742
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-13 23:48:27 +0530

    [+] login function implemented with redux

--- V2/Client/client/src/pages/login/login.js
+++ V2/Client/client/src/pages/login/login.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { Container, Row, Col, Button, Image, Form } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Image,
+  Form,
+  Modal,
+  Alert,
+} from "react-bootstrap";
+import PropTypes from "prop-types";
 import img from "../../assets/login-form.svg";
 import "./login.scss";
 import Navbar from "../../components/navbar/navbar";
 
-function Login() {
+import { connect } from "react-redux";
+import { loginUser } from "../../redux/actions/userAction";
+
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
+
+  const {
+    UI: { loading },
+  } = props;
+
+  useEffect(() => {
+    props.UI.errors && setErrors(props.UI.errors.message);
+  }, [props.UI.errors]);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const data = { email, password };
+    props.loginUser(data, props.history);
+  };
+
   return (
     <>
       <Navbar />
@@ -14,25 +45,47 @@ function Login() {
             <Image src={img} alt="books" className="booklogin" />
           </Col>
           <Col className="form-box">
-            <Form className="login-form">
+            <Form className="login-form" onSubmit={handleLogin}>
+              <Modal.Title>Welcome back!</Modal.Title>
               <Form.Text className="text-muted">
-                <h2>Welcome back.</h2>
+                <h6> </h6>
               </Form.Text>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Text className="text-muted">forget password?</Form.Text>
               </Form.Group>
+
+              <Alert variant="danger" hidden={!errors}>
+                {!errors ? "error" : errors}
+              </Alert>
               <div className="d-grid gap-2">
-                <Button variant="outline-primary" type="submit" size="lg">
-                  Login
+                <Button
+                  variant="outline-primary"
+                  type="submit"
+                  size="lg"
+                  disabled={loading}
+                >
+                  {loading ? "Logging in..." : "Login"}
                 </Button>
               </div>
             </Form>
@@ -43,4 +96,19 @@ function Login() {
   );
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI,
+});
+
+const mapActionsToProps = {
+  loginUser,
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Login);
