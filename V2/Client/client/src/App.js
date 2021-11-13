e7a807f1484e8ecf7e398fbd22fa378bb8c84054
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-13 23:44:51 +0530

    [+]Auth route for pages only unauthenticated users

--- V2/Client/client/src/App.js
+++ V2/Client/client/src/App.js
@@ -1,30 +1,59 @@
 import "./App.scss";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Fragment } from "react";
 import axios from "axios";
+import jwtDecode from "jwt-decode";
 
-import Home from "./pages/home/home";
+//redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userAction";
+
+//utils
+import AuthRoute from "./util/authRoute";
+
+import Landing from "./pages/landingPage/landingPage";
 import Login from "./pages/login/login";
 import Signup from "./pages/signUp/signUp";
 import UserImgUpload from "./pages/signUp/imageForm";
-
+import Home from "./pages/home/home";
+import Books from "./pages/books/books";
+import Dashboard from "./pages/dashboard/dashboard";
 
 axios.defaults.baseURL = "http://localhost:3001";
+
+const token = localStorage.LenderToken;
+
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
+    window.location.href = "user/login";
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
+    store.dispatch(getUserData());
+  }
+}
 
 function App() {
   return (
     <>
       <Fragment>
-      
+        <Provider store={store}>
           <Router>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/user/login" element={<Login />} />
-              <Route exact path="/user/register" element={<Signup />} />
-              <Route exact path="/img-upload" element={<UserImgUpload />} />
-            </Routes>
+            <Switch>
+              <Route exact path="/welcome" component={Landing} />
+              <AuthRoute exact path="/user/login" component={Login} />
+              <Route exact path="/user/register" component={Signup} />
+              <Route exact path="/uploadImage" component={UserImgUpload} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/lend-books" component={Books} />
+              <Route exact path="/Admin-dashboard" component={Dashboard} />
+            </Switch>
           </Router>
-       
+        </Provider>
       </Fragment>
     </>
   );
