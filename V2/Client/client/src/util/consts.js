93195f704b84942e78de5dee5247037dd361f4bf
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-26 15:51:52 +0530

    [+] movie categories

--- V2/Client/client/src/util/consts.js
+++ V2/Client/client/src/util/consts.js
@@ -6,3 +6,14 @@ exports.BOOK_CATRGORIES = [
   { id: "sci_fi", name: "Sci-Fi" },
   { id: "biographies ", name: "Biographies " },
 ];
+
+
+exports.MOVIE_CATRGORIES = [
+  { id: "action", name: "Action" },
+  { id: "adventure ", name: "Adventure " },
+  { id: "animated ", name: "Animated " },
+  { id: "comedy", name: "Comedy" },
+  { id: "horror", name: "Horror" },
+  { id: "sci_fi", name: "Sci-Fi" },
+  
+];
